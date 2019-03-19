2386b8dbaf5b39c4b6f2b868472530a73ea3b696
MAGOW <marco.orozco@goldenhippo.com>
2019-03-18 17:27:51 -0700

    dorcker files to use

--- index.php
+++ index.php
@@ -62,7 +62,26 @@ if ($tablet_browser > 0) {
         <style>
         #play-me{position: absolute; z-index: 9; top: 3em; left: 9em; cursor: pointer;}
 		#play-me img{position: realtive;  z-index: 20; cursor: pointer; width:103px}
-		#LimelightEmbeddedPlayerFlash{position: relative; z-index: 0; cursor: pointer; width: 100%;}
+		#Lime# Use an official Python runtime as a parent image
+FROM python:2.7-slim
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]lightEmbeddedPlayerFlash{position: relative; z-index: 0; cursor: pointer; width: 100%;}
 		#videoNext{padding: 10px 0;}
 		.oo-player-container{width:100%; min-width: 0 !important;}
 		.oo-player-container .oo-start-screen .oo-start-screen-linear-gradient{background: transparent !important;}
